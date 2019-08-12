54b57d429ef5a08fa76a52d89ed762263de93275
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-08-12 15:59:03 +0200

    wip: simulation advance()

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,7 +1,44 @@
 #include "simulation.h"
-
 #include "math.h"
 
+Simulation::Simulation(const RigidBody& initialRB,
+                       const MagneticField& initialMF) :
+    rigidBody(initialRB),
+    magneticField(initialMF)
+{}
 
+real3 operator*(const PropulsionMatrix::SubMatrix& A, const real3& v)
+{
+    return {A[0] * v.x,
+            A[1] * v.y,
+            A[2] * v.z};
+}
 
+static inline std::tuple<real3, real3> computeVelocities(const PropulsionMatrix& m,
+                                                         const real3& F, const real3& T)
+{
+    const real3 v = m.A * F + m.B * T;
+    const real3 w = m.B * F + m.C * T;
+    return {v, w};
+}
 
+void Simulation::advance(long nsteps, real dt)
+{
+    auto q = rigidBody.q;
+    
+    const real3 B      = magneticField(t);
+    const real3 m      = q.rotate(rigidBody.magnMoment);
+    const real3 torque = cross(m, B);
+    constexpr real3 force {0.0_r, 0.0_r, 0.0_r};
+
+    std::tie(rigidBody.v, rigidBody.omega) =
+        computeVelocities(rigidBody.propulsion, force, torque);
+
+    const Quaternion _omega (0.0_r, rigidBody.omega);
+    const auto dq_dt = 0.5_r * q * _omega;
+
+    rigidBody.r += dt * rigidBody.v;
+    q += dt * dq_dt;
+
+    rigidBody.q = q.normalized();
+}
--- src/simulation.h
+++ src/simulation.h
@@ -3,17 +3,26 @@
 #include "quaternion.h"
 #include "types.h"
 
+#include <array>
 #include <cmath>
+
+struct PropulsionMatrix
+{
+    using SubMatrix = std::array<real,3>;
+    SubMatrix A, B, C;
+};
 
 struct RigidBody
 {
     Quaternion q;
-    real3 r, v, omega;
+    real3 r, v, omega, magnMoment;
+    PropulsionMatrix propulsion;
 };
 
 struct MagneticField
 {
     real magnitude, omega;
+    // real3 rotatingDirection;
     Quaternion q;
     
     real3 operator()(real t) const
@@ -30,11 +39,16 @@ class Simulation
 {
 public:
 
-    Simulation();
+    Simulation(const RigidBody& initialRB, const MagneticField& initialMF);
+    ~Simulation() = default;
+
+    Simulation(const Simulation&) = default;
+    Simulation& operator=(const Simulation&) = default;
     
     void advance(long nsteps, real dt);
     
 private:
+    real t {0.0_r};
     RigidBody rigidBody;
     MagneticField magneticField;
 };
