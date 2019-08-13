e4bae9c87472a5bda62908a0532c9c0d4eb97588
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-08-13 09:33:16 +0200

    play with parameters

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,15 @@
 
 int main(int argc, char **argv)
 {
-    const real magneticMomentMagnitude {0.01_r};
+    const real magneticMomentMagnitude {10.0_r};
 
     const real magneticFieldMagnitude {1.0_r};
     const real omegaField {0.5_r};
+
     auto rotatingDirection = [&](real t)
     {
         const real omega = 0.01_r;
-        const real wt_2 = 0.5_r * t * omega;
+        const real wt_2  = 0.5_r * t * omega;
         constexpr real3 original {1._r, 0._r, 0._r};
         constexpr real3 axis {0._r, 1._r, 0._r};
         const Quaternion q{std::cos(wt_2), std::sin(wt_2) * axis};
@@ -18,8 +19,10 @@ int main(int argc, char **argv)
     
     const Quaternion initialOrientation {0._r, 1._r, 0._r, 0._r};
     const real3 initialPosition{0._r, 0._r, 0._r};
-    const real3 magneticMoment{magneticMomentMagnitude, 0._r, 0._r};
-    const PropulsionMatrix propulsion {{1._r, 1._r, 1._r}, {1._r, 0._r, 0._r}, {1._r, 1._r, 1._r}};
+    const real3 magneticMoment{0.0_r, magneticMomentMagnitude, 0._r};
+    const PropulsionMatrix propulsion {{1._r, 1._r, 1._r},
+                                       {1._r, 0._r, 0._r},
+                                       {1._r, 0.9_r, 0.9_r}};
     
     RigidBody rigid {initialOrientation,
                      initialPosition,
@@ -30,7 +33,7 @@ int main(int argc, char **argv)
     
     Simulation simulation(rigid, magneticField);
 
-    const real dt {0.001_r};
+    const real dt {0.01_r};
     const long nsteps {100000};
     simulation.advance(nsteps, dt);
     
