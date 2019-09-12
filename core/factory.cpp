4b491be39187e5df36885a68a12528852aae33d1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-12 11:19:27 +0200

    use parser in factory

--- core/factory.cpp
+++ core/factory.cpp
@@ -1,4 +1,5 @@
 #include "factory.h"
+#include "file_parser.h"
 #include "log.h"
 
 #include <map>
@@ -6,67 +7,22 @@
 #include <fstream>
 #include <sstream>
 
-using DataMap = std::map<std::string, std::string>;
-
-static DataMap readFile(const std::string& fname)
+static PropulsionMatrix getPropulsion(const FileParser& parser)
 {
-    DataMap dataMap;
-    std::ifstream file(fname);
-    Expect(file.is_open(), "could not open file for read");
-
-    for (std::string line {}; std::getline(file, line); )
-    {
-        std::istringstream iss(line);
-        std::string key, tmp, data;
-
-        iss >> key;
-        while (iss >> tmp) {data += " " + tmp;}
-        dataMap[key] = data;
-    }
-    return dataMap;
-}
-
-static PropulsionMatrix::SubMatrix getSubMatrix(const std::string& str)
-{
-    PropulsionMatrix::SubMatrix M;
-    std::istringstream iss(str);
-    iss >> M[0] >> M[1] >> M[2];
-    return M;
-}
-
-static real3 getReal3(const std::string& str)
-{
-    real3 v;
-    std::istringstream iss(str);
-    iss >> v.x >> v.y >> v.z;
-    return v;
-}
-
-static Quaternion getQuaternion(const std::string& str)
-{
-    real w;
-    real3 u;
-    std::istringstream iss(str);
-    iss >> w >> u.x >> u.y >> u.z;
-    return Quaternion::createFromComponents(w, u);
-}
-
-static PropulsionMatrix getPropulsion(DataMap& dataMap)
-{
-    return {getSubMatrix(dataMap["A"]),
-            getSubMatrix(dataMap["B"]),
-            getSubMatrix(dataMap["C"])};
+    return {parser.getSubMatrix("A"),
+            parser.getSubMatrix("B"),
+            parser.getSubMatrix("C")};
 }
 
 namespace Factory
 {
 RigidBody readRigidBodyConfig(const std::string& fname)
 {
-    auto data = readFile(fname);
-    const PropulsionMatrix propulsion = getPropulsion(data);
-    const Quaternion q = getQuaternion(data["q"]);
-    const real3 r = getReal3(data["r"]);
-    const real3 m = getReal3(data["m"]);
+    const FileParser parser(fname);
+    const PropulsionMatrix propulsion = getPropulsion(parser);
+    const Quaternion q = parser.getQuaternion("q");
+    const real3 r = parser.getReal3("r");
+    const real3 m = parser.getReal3("m");
 
     return {q, r, m, propulsion};
 }
