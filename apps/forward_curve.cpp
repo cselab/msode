71293e12a25873551471d70f9416db2f4be364af
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-13 11:04:22 +0200

    add simple app to compute the forward curve of swimmers

--- apps/forward_curve.cpp
+++ apps/forward_curve.cpp
@@ -1,40 +1,50 @@
 #include "simulation.h"
 #include "factory.h"
 
-int main(int argc, char **argv)
+#include <iostream>
+
+static inline real meanVelocity(real3 r0, real3 r1, real T)
 {
-    Expect(argc > 1, "usage : ./main <config0> <config1>...");
+    return length(r0-r1)/T;
+}
 
-    std::vector<RigidBody> rigidBodies;
-    
-    for (int i = 1; i < argc; ++i)
-        rigidBodies.push_back(Factory::readRigidBodyConfig(argv[i]));
-    
+static real computeMeanVelocity(RigidBody body, real omega)
+{
+    constexpr real tEnd = 200.0_r;
+    constexpr real dt {1e-3_r};
+    constexpr long nsteps = tEnd / dt;
+
+    constexpr real3 rStart {0.0_r, 0.0_r, 0.0_r};
+    body.r = rStart;
+        
     const real magneticFieldMagnitude {1.0_r};
-    auto omegaField = [](real t) {return 0.5_r;};
-
-    auto rotatingDirection = [](real t)
-    {
-        const real omega = 0.01_r;
-        const real wt_2  = 0.5_r * t * omega;
-        constexpr real3 original {1._r, 0._r, 0._r};
-        constexpr real3 axis {0._r, 0._r, 1._r};
-        const auto q = Quaternion::createFromRotation(wt_2, axis);
-        return q.rotate(original);
-    };
+    auto omegaField        = [omega](real t) {return omega;};
+    auto rotatingDirection = []     (real t) {return real3{1.0_r, 0.0_r, 0.0_r};};
 
     MagneticField magneticField {magneticFieldMagnitude, omegaField, rotatingDirection};
-    
+    const std::vector<RigidBody> rigidBodies {body};
     Simulation simulation {rigidBodies, magneticField};
 
-    const real tEnd = 2000.0_r;
-    const real tDump = 0.1_r;
-    const real dt {0.001_r};
-    const long nsteps = tEnd / dt;
+    simulation.run(nsteps, dt);
 
-    simulation.activateDump("out.txt", tDump / dt);
-    simulation.run(nsteps, dt);
-    simulation.run(nsteps, dt);
+    const real3 rEnd = simulation.getBodies()[0].r;
+
+    return meanVelocity(rStart, rEnd, tEnd);
+}
+
+int main(int argc, char **argv)
+{
+    Expect(argc == 2, "usage : ./main swimmer.cfg");
+
+    const auto body = Factory::readRigidBodyConfig(argv[1]);
+
+    const int nOmegas = 200;
+    for (int i = 0; i < nOmegas; ++i)
+    {
+        const real omega = i * 0.1_r;
+        const real v = computeMeanVelocity(body, omega);
+        std::cout << omega << " " << v << std::endl;
+    }
     
     return 0;
 }
