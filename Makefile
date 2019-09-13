300f2b81d54d6f0998c86a5cb0ca7d8da83316f1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-13 10:47:32 +0200

    move apps in their own folder

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 CORE_DIR=core
+APPS_DIR=apps
 RL_DIR=rl
 
-all: msode_rl msode
-
-msode:
-	@make -C $(CORE_DIR) main
-	@cp $(CORE_DIR)/main $@
+all: msode_rl apps
 
 libmsode.a:
 	@make -C $(CORE_DIR) $@
+
+apps:
+	@make -C $(APPS_DIR) all
 
 msode_rl: libmsode.a
 	@make -C $(RL_DIR) main
@@ -16,7 +16,8 @@ msode_rl: libmsode.a
 
 clean:
 	make -C $(CORE_DIR) clean
+	make -C $(APPS_DIR) clean
 	make -C $(RL_DIR)   clean
-	rm -rf msode msode_rl test_environment
+	rm -rf msode_rl
 
-.PHONY: all clean
+.PHONY: all clean apps
---
+++ apps/rotating.cpp
@@ -0,0 +1,40 @@
+#include "simulation.h"
+#include "factory.h"
+
+int main(int argc, char **argv)
+{
+    Expect(argc > 1, "usage : ./main <config0> <config1>...");
+
+    std::vector<RigidBody> rigidBodies;
+    
+    for (int i = 1; i < argc; ++i)
+        rigidBodies.push_back(Factory::readRigidBodyConfig(argv[i]));
+    
+    const real magneticFieldMagnitude {1.0_r};
+    auto omegaField = [](real t) {return 0.5_r;};
+
+    auto rotatingDirection = [](real t)
+    {
+        const real omega = 0.01_r;
+        const real wt_2  = 0.5_r * t * omega;
+        constexpr real3 original {1._r, 0._r, 0._r};
+        constexpr real3 axis {0._r, 0._r, 1._r};
+        const auto q = Quaternion::createFromRotation(wt_2, axis);
+        return q.rotate(original);
+    };
+
+    MagneticField magneticField {magneticFieldMagnitude, omegaField, rotatingDirection};
+    
+    Simulation simulation {rigidBodies, magneticField};
+
+    const real tEnd = 2000.0_r;
+    const real tDump = 0.1_r;
+    const real dt {0.001_r};
+    const long nsteps = tEnd / dt;
+
+    simulation.activateDump("out.txt", tDump / dt);
+    simulation.run(nsteps, dt);
+    simulation.run(nsteps, dt);
+    
+    return 0;
+}
