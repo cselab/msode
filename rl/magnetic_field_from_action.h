85b56a94601f7eff9dec67adf5ae473f1cb5a2a9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-16 15:02:32 +0200

    mv impl to header

--- rl/magnetic_field_from_action.h
+++ rl/magnetic_field_from_action.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <types.h>
+#include <log.h>
 #include <math.h>
+#include <types.h>
 
 #include <vector>
 
@@ -12,11 +13,38 @@ struct MagnFieldFromActionChange
         actionDt(actionDt)
     {}
 
-    void setAction(const std::vector<double>& action);
+    void setAction(const std::vector<double>& action)
+    {
+        Expect(action.size() == 4, "expect action of size 4");
+        dOmega = action[0];
+        dAxis.x = action[1];
+        dAxis.y = action[2];
+        dAxis.z = action[3];
+    }
 
-    void advance(real t);
-    real getOmega(real t) const;
-    real3 getAxis(real t) const;
+    void advance(real t)
+    {
+        // advance
+        lastOmega += omegaActionChange(t);
+        lastAxis += axisActionChange(t);
+        lastActionTime = t;
+
+        // constraints
+        lastOmega = std::max(lastOmega, 0._r);
+        lastOmega = std::min(lastOmega, maxOmega);
+        lastAxis = normalized(lastAxis);
+    }
+
+    real getOmega(real t) const
+    {
+        return lastOmega + omegaActionChange(t);
+    }
+
+    real3 getAxis(real t) const
+    {
+        return lastAxis + axisActionChange(t);
+    }
+
 
 private:
     const real maxOmega;
@@ -29,9 +57,22 @@ private:
     real dOmega {0._r};
     real3 dAxis {0._r, 0._r, 0._r};
 
-    real omegaActionChange(real t) const;
-    real3 axisActionChange(real t) const;
+    static inline real transitionSmoothKernel(real x) { return x * x * (3.0_r - 2.0_r * x); }
+    static inline real transitionLinearKernel(real x) { return x; }
+
+    real omegaActionChange(real t) const
+    {
+        const real tau = (t - lastActionTime) / actionDt;
+        return transitionLinearKernel(tau) * dOmega;
+    }
+
+    real3 axisActionChange(real t) const
+    {
+        const real tau = (t - lastActionTime) / actionDt;
+        return transitionSmoothKernel(tau) * dAxis;
+    }
 };
+
 
 struct MagnFieldFromActionDirect
 {
@@ -40,11 +81,23 @@ struct MagnFieldFromActionDirect
         actionDt(actionDt)
     {}
 
-    void setAction(const std::vector<double>& action);
+    void setAction(const std::vector<double>& action)
+    {
+        Expect(action.size() == 4, "expect action of size 4");
+        constexpr real tolerance = 1e-6_r;
+    
+        omega = action[0];
+        axis.x = action[1];
+        axis.y = action[2];
+        axis.z = action[3];
+        if (dot(axis, axis) < tolerance)
+            axis.x = 1.0_r;
+        axis = normalized(axis);
+    }
 
-    void advance(real t);
-    real getOmega(real t) const;
-    real3 getAxis(real t) const;
+    void advance(real t) {}
+    real getOmega(real t) const  {return omega;}
+    real3 getAxis(real t) const  {return axis;}
 
 private:
     const real maxOmega;
