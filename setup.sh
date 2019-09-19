2b21cf81e785d00753339c166788bc608dbb9937
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-19 14:55:59 +0200

    use "real" data

--- setup.sh
+++ setup.sh
@@ -10,8 +10,14 @@ MSODE_ROOT=${SMARTIES_ROOT}/../msode
 cp $MSODE_ROOT/msode_rl $RUNDIR/exec
 
 # copy config
+# CFG_DIR=$RUNDIR/config
+# mkdir -p $RUNDIR/config
+# cp $MSODE_ROOT/config/swimmer*cfg $CFG_DIR/
+# echo "0 $CFG_DIR/swimmer0.cfg" >  $CFG_DIR/swimmers_list.cfg
+# echo "1 $CFG_DIR/swimmer1.cfg" >> $CFG_DIR/swimmers_list.cfg 
+
 CFG_DIR=$RUNDIR/config
 mkdir -p $RUNDIR/config
-cp $MSODE_ROOT/config/swimmer*cfg $CFG_DIR/
-echo "0 $CFG_DIR/swimmer0.cfg" >  $CFG_DIR/swimmers_list.cfg
-echo "1 $CFG_DIR/swimmer1.cfg" >> $CFG_DIR/swimmers_list.cfg 
+cp $MSODE_ROOT/data/config/swimmer*cfg $CFG_DIR/
+echo "0 $CFG_DIR/swimmer_00.cfg" >  $CFG_DIR/swimmers_list.cfg
+echo "1 $CFG_DIR/swimmer_01.cfg" >> $CFG_DIR/swimmers_list.cfg 
