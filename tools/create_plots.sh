920a9858e01ea3d5835944680e4f34ea17d4404f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-20 09:28:37 +0200

    more robust script

--- tools/create_plots.sh
+++ tools/create_plots.sh
@@ -8,7 +8,11 @@ mkdir -p $out_dir
 
 for i in `seq 0 $n`; do
     id=`printf "%02d" $i`
+
+    tmp=/tmp/trajectory.txt
+    head -n -1 eval_$name/simulation_001_000$id/trajectories.txt > $tmp
+    
     mir.post ./tools/plot_trajectories.py \
-	     --file eval_$name/simulation_001_000$id/trajectories.txt \
+	     --file $tmp  \
 	     --out $out_dir/$id.pdf
 done 
