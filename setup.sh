757ff2496de87ac206eb749269cba3e8882687e5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-20 13:46:11 +0200

    minor

--- setup.sh
+++ setup.sh
@@ -16,4 +16,5 @@ data_dir=data/test
 mkdir -p $RUNDIR/config
 cp $MSODE_ROOT/$data_dir/config/swimmer*cfg $CFG_DIR/
 echo "0 $CFG_DIR/swimmer_00.cfg" >  $CFG_DIR/swimmers_list.cfg
-echo "1 $CFG_DIR/swimmer_01.cfg" >> $CFG_DIR/swimmers_list.cfg 
+echo "1 $CFG_DIR/swimmer_01.cfg" >> $CFG_DIR/swimmers_list.cfg
+#echo "2 $CFG_DIR/swimmer_02.cfg" >> $CFG_DIR/swimmers_list.cfg 
