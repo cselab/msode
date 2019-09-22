15190a9ab06362e32ad5ed4a5c67d970bb662e27
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-22 20:15:19 +0200

    update helix data

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ cp $MSODE_ROOT/msode_rl $RUNDIR/exec
 
 # copy config
 CFG_DIR=$RUNDIR/config
-#data_dir=data/real
-data_dir=data/test
+#data_dir=data/test
+data_dir=data/helix
 mkdir -p $RUNDIR/config
 cp $MSODE_ROOT/$data_dir/config/swimmer*cfg $CFG_DIR/
 echo "0 $CFG_DIR/swimmer_00.cfg" >  $CFG_DIR/swimmers_list.cfg
 echo "1 $CFG_DIR/swimmer_01.cfg" >> $CFG_DIR/swimmers_list.cfg
-#echo "2 $CFG_DIR/swimmer_02.cfg" >> $CFG_DIR/swimmers_list.cfg 
+
