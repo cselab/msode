cf26a884a8745294a87f22408d7cf4d6af97b805
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-09-23 11:59:16 +0200

    option for step out frequency

--- tools/plot_forward.py
+++ tools/plot_forward.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser()
 parser.add_argument('--files', type=str, required=True, nargs='+', help='output of ODE simulation (forward_curve)')
 parser.add_argument('--out', type=str, default="GUI")
+parser.add_argument('--show_omegac', action='store_true', default=False)
 args = parser.parse_args()
 
 fig = plt.figure(0)
@@ -17,6 +18,19 @@ for filename in args.files:
     w = data[:,0]
     v = data[:,1]
     ax.plot(w, v, '-')
+
+    if args.show_omegac:
+        i = np.argmax(v)
+        wc = w[i]
+        vmax = v[i]
+        ax.plot([wc,wc], [0,vmax], '--k')
+        locs   = ax.get_xticks()
+        labels = [str(x) for x in locs]
+        locs   = np.append(locs, wc)
+        labels = np.append(labels, r'$\omega_c$')
+        
+        ax.set_xticks(locs)
+        ax.set_xticklabels(labels)
     
 ax.set_xlabel(r'$\omega$ [Hz]')
 ax.set_ylabel(r'$V$ [body length / s]')
