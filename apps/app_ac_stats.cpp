e8316233883e8dbd5ea92303b336862f99351fb5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-12-09 17:14:34 +0100

    pass out file

--- apps/app_ac_stats.cpp
+++ apps/app_ac_stats.cpp
@@ -7,22 +7,24 @@ int main(int argc, char **argv)
 {
     using namespace msode;
     
-    if (argc < 3                     ||
+    if (argc < 4                     ||
         std::string(argv[1]) == "-h" ||
         std::string(argv[1]) == "--help")
     {
-        fprintf(stderr, "usage : %s <nsamples> <swimmer0.cfg> <swimmer1.cfg>... \n\n", argv[0]);
+        fprintf(stderr, "usage : %s <nsamples> <outfile> <swimmer0.cfg> <swimmer1.cfg>... \n\n", argv[0]);
         return 1;
     }
 
     const int nsamples = atoi(argv[1]);
+    const char *outFilename = argv[2];
+    
     const real magneticFieldMagnitude = 1.0_r;
 
     const real3 boxLo{-50.0_r, -50.0_r, -50.0_r};
     const real3 boxHi{+50.0_r, +50.0_r, +50.0_r};
     
     std::vector<RigidBody> bodies;
-    for (int i = 2; i < argc; ++i)
+    for (int i = 3; i < argc; ++i)
     {
         const auto body = Factory::readRigidBodyConfig(argv[i]);
         bodies.push_back(body);
@@ -33,6 +35,7 @@ int main(int argc, char **argv)
 
     real tSum = 0.0_r;
 
+    FILE *fout = fopen(outFilename, "w");
     for (int sample = 0; sample < nsamples; ++sample)
     {
         const long seed = 242 * sample + 13;
@@ -42,10 +45,11 @@ int main(int argc, char **argv)
         tSum += t;
 
         for (auto r : initialPositions)
-            printf("%g ", length(r));
-        printf("%g\n", t);
+            fprintf(fout, "%g ", length(r));
+        fprintf(fout, "%g\n", t);
     }
-
+    fclose(fout);
+    
     const real tMean = tSum / nsamples;
 
     printf("Average time %g\n", tMean);
