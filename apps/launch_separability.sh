44571b7add2c4d9eb93c243730a5b6cb0a2b3820
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-12-12 10:16:11 +0100

    up to 5

--- apps/launch_separability.sh
+++ apps/launch_separability.sh
@@ -3,7 +3,7 @@
 base="sep"
 nsamples=1000
 
-for N in `seq 2 4`; do
+for N in `seq 2 5`; do
     echo "starting N = $N"
     ./app_ac_separability $N 50.0 $nsamples > ${base}_N_${N}.txt
     echo done
