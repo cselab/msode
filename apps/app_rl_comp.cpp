80e16dd54206b498ad0f503d67c7171c7fe764a0
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-12-16 14:27:30 +0100

    dump also initial distance

--- apps/app_rl_comp.cpp
+++ apps/app_rl_comp.cpp
@@ -23,15 +23,28 @@ static inline std::vector<real3> extractPositions(const std::vector<RigidBody>& 
     return positions;
 }
 
-static inline void dumpComparisonInfos(std::ostream& stream, int simId, real timeAC, real timeRL, const std::vector<RigidBody>& bodiesRL)
+static inline real getDistance(const RigidBody& b) {return length(b.r);}
+
+static real computeMinDistance(const std::vector<RigidBody>& bodies)
 {
-    auto getDistance = [](const RigidBody& b) {return length(b.r);};
-    
+    real minDistance = std::numeric_limits<real>::max();
+    for (auto b : bodies)
+        minDistance = std::min(minDistance, getDistance(b));
+    return minDistance;
+}
+
+static real computeMaxDistance(const std::vector<RigidBody>& bodies)
+{
     real maxDistance = 0;
-    for (auto b : bodiesRL)
+    for (auto b : bodies)
         maxDistance = std::max(maxDistance, getDistance(b));
-    
-    stream << simId << " " << timeAC << " " << timeRL << " " << maxDistance << std::endl;
+    return maxDistance;
+}
+
+static inline void dumpComparisonInfos(std::ostream& stream, int simId, real timeAC, real timeRL, const std::vector<RigidBody>& bodiesRL, real initDistance)
+{
+    const real maxDistance = computeMaxDistance(bodiesRL);
+    stream << simId << " " << timeAC << " " << timeRL << " " << maxDistance << " " << initDistance << std::endl;
 }
 
 
@@ -67,6 +80,7 @@ inline void appMain(smarties::Communicator *const comm, int /*argc*/, char **/*a
         env->reset(simId, comm->getPRNG());
         comm->sendInitState(env->getState());
 
+        const real initDistance = computeMinDistance(env->getBodies());
         const real tAC =  [&]()
         {
             const auto envBodies = env->getBodies();
@@ -95,7 +109,7 @@ inline void appMain(smarties::Communicator *const comm, int /*argc*/, char **/*a
 
                 const auto envBodies = env->getBodies();
                 const real tRL = env->getSimulationTime();
-                dumpComparisonInfos(std::cout, simId, tAC, tRL, envBodies);
+                dumpComparisonInfos(std::cout, simId, tAC, tRL, envBodies, initDistance);
             }
         }
 
