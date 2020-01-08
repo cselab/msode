e24e65df2435d78a00e5064a601b482a67c87cca
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-08 17:29:24 +0100

    minor: more data

--- apps/launch_separability.sh
+++ apps/launch_separability.sh
@@ -5,7 +5,7 @@ nsamples=1000
 
 ae () { awk 'BEGIN {printf "%g", '"$@"'}'; }
 
-for N in `seq 2 10`; do
+for N in `seq 2 12`; do
     omegaMax=`ae "$N * 25.0"`
     echo "starting N = $N with wmax = $omegaMax"
     ./app_ac_separability $N $omegaMax $nsamples > ${base}_N_${N}.dat
