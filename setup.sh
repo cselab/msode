21a8036331e66a50f4dc45eb14d28ffde5056546
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-09 08:39:52 +0100

    minor

--- setup.sh
+++ setup.sh
@@ -15,10 +15,16 @@ cp $exec $RUNDIR/exec
 
 # copy config
 CFG_DIR=$RUNDIR/config
+mkdir -p $CFG_DIR
+
 #data_dir=data/test
 data_dir=data/helix
-mkdir -p $RUNDIR/config
+
 cp $MSODE_ROOT/$data_dir/config/swimmer*cfg $CFG_DIR/
-echo "0 $CFG_DIR/swimmer_00.cfg" >  $CFG_DIR/swimmers_list.cfg
-echo "1 $CFG_DIR/swimmer_01.cfg" >> $CFG_DIR/swimmers_list.cfg
+
+swimmer_list=$CFG_DIR/swimmers_list.cfg
+rm -rf $swimmer_list
+for i in 0 1; do
+    echo "$i $CFG_DIR/swimmer_0$i.cfg" >> $swimmer_list
+done
 
