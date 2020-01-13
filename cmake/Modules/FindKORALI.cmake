53d7e68244034e51fa0175313b798ec71507a080
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-13 11:13:08 +0100

    attempt to parse korali tools

--- cmake/Modules/FindKORALI.cmake
+++ cmake/Modules/FindKORALI.cmake
@@ -15,36 +15,49 @@ execute_process(
 if (KORALI_EXISTS EQUAL "0")
   separate_arguments(KORALI_FLAGS UNIX_COMMAND "${KORALI_FLAGS_RAW}")
   separate_arguments(KORALI_LIBS UNIX_COMMAND "${KORALI_LIBS_RAW}")
+
+  list(REMOVE_DUPLICATES KORALI_FLAGS)
+  list(REMOVE_DUPLICATES KORALI_LIBS)
 endif()
 
-set(KORALI_INCLUDES ${KORALI_FLAGS})
-list(FILTER KORALI_FLAGS    EXCLUDE REGEX "-I.*")
+find_package_handle_standard_args(KORALI
+  FOUND_VAR
+    KORALI_FOUND
+  REQUIRED_VARS
+    KORALI_FLAGS
+    KORALI_LIBS
+)
 
-list(FILTER KORALI_INCLUDES INCLUDE REGEX "-I.*")
-list(TRANSFORM KORALI_INCLUDES REPLACE "-I" "")
+if (KORALI_FOUND)
+    set(KORALI_INCLUDES "")
+  set(KORALI_DEFINITIONS "")
 
-message("includes: ${KORALI_INCLUDES}")
-message("cflags: ${KORALI_FLAGS}")
-message("libs:   ${KORALI_LIBS}")
+  foreach(flag IN LISTS KORALI_FLAGS)
+    if (flag MATCHES "^-I.*")
+      string (REGEX REPLACE "^-I" "" include_path ${flag})
+      if (EXISTS ${include_path})
+	list (APPEND KORALI_INCLUDES ${include_path})
+      endif()
+    elseif (flag MATCHES "^-D.*")
+      list (APPEND KORALI_DEFINITIONS ${flag})
+    else()
+      # for now ignore all the rest
+      # message("skip ${flag}")
+    endif()
+  endforeach(flag)
 
+  #list(REMOVE_ITEM KORALI_LIBS "-lkorali")
 
+  MARK_AS_ADVANCED(
+    KORALI_FLAGS_RAW
+    KORALI_LIBS_RAW
+    )
 
-# find_package_handle_standard_args(
-#   KORALI DEFAULT_MSG
-#   KORALI_TOKEN
-#   KORALI_LIBS
-#   )
+  add_library(korali SHARED IMPORTED)
 
-set(SMARTIES_INCLUDE_DIRS ${SMARTIES_INCLUDE_DIR})
-set(SMARTIES_LIBRARIES ${SMARTIES_LIBRARY})
+  target_include_directories(korali INTERFACE ${KORALI_INCLUDES})
+  #target_compile_options(korali INTERFACE ${KORALI_FLAGS})
+  target_compile_definitions(korali INTERFACE ${KORALI_DEFINITIONS})
 
-MARK_AS_ADVANCED(
-  KORALI_FLAGS_RAW
-  KORALI_LIBS_RAW
-  )
-
-add_library(korali SHARED IMPORTED)
-
-target_include_directories(korali INTERFACE ${KORALI_INCLUDES})
-target_compile_options(korali INTERFACE ${KORALI_FLAGS})
-target_link_libraries(korali INTERFACE ${KORALI_LIBS})
+  #target_link_libraries(korali INTERFACE ${KORALI_LIBS})
+endif()
