65478b992870066ad44a560cd3cf1fc9b2a424d9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-15 15:49:11 +0100

    add msode_die

--- src/msode/log.h
+++ src/msode/log.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdio>
 #include <iostream>
 #include <string>
 
@@ -17,3 +18,18 @@
 
 #define MSODE_Expect(val, msg) MSODE_CHECK(val, std::string(__FILE__) + ":" + std::to_string(__LINE__) + ": Failed Expect : " + msg)
 #define MSODE_Ensure(val, msg) MSODE_CHECK(val, std::string(__FILE__) + ":" + std::to_string(__LINE__) + ": Failed Ensure : " + msg)
+
+namespace msode
+{
+
+template <class... Args>
+static inline void die(const char *file, int line, const std::string& pattern, Args... args)
+{
+    fprintf(stderr, "[msode] an error occured on %s:%d:\n", file, line);
+    fprintf(stderr, pattern.c_str(), args...);
+    exit(1);
+}
+
+} // namespace msode
+
+#define msode_die(pattern, ...) msode::die(__FILE__, __LINE__, pattern, ##__VA_ARGS__)
