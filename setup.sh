315742520032ce25f57e1e398f0ada66917b3710
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-15 10:45:14 +0100

    adapt setup

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@
 MSODE_ROOT=${SMARTIES_ROOT}/../msode
 
 # copy executable:
-#exec=$MSODE_ROOT/apps/app_rl
-#exec=$MSODE_ROOT/apps/app_rl_comp
-exec=$MSODE_ROOT/apps/app_rl_curriculum
+exec=$MSODE_ROOT/build/apps/app_rl
+#exec=$MSODE_ROOT/build/apps/app_rl_comp
+#exec=$MSODE_ROOT/build/apps/app_rl_curriculum
 
 cp $exec $RUNDIR/exec
 
@@ -19,11 +19,13 @@ mkdir -p $CFG_DIR
 
 #data_dir=data/test
 data_dir=data/helix
+#data_dir=data/helix_many
 
 cp $MSODE_ROOT/$data_dir/config/swimmer*cfg $CFG_DIR/
 
 swimmer_list=$CFG_DIR/swimmers_list.cfg
 rm -rf $swimmer_list
+#for i in 0 4 8; do
 for i in 0 1; do
     echo "$i $CFG_DIR/swimmer_0$i.cfg" >> $swimmer_list
 done
