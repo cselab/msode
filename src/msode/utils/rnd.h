e9770264f3083ecd5798ccdbdab8e8e3e5dc3771
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-16 15:29:45 +0100

    minor

--- src/msode/utils/rnd.h
+++ src/msode/utils/rnd.h
@@ -7,8 +7,6 @@
 namespace msode {
 namespace utils {
 
-using namespace msode::literals;
-
 msode::real3 generateUniformPositionBox(std::mt19937& gen, msode::real3 lo, msode::real3 hi);
 msode::real3 generateUniformPositionBall(std::mt19937& gen, msode::real radius = 1.0_r);
 msode::Quaternion generateUniformQuaternion(std::mt19937& gen);
