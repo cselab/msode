6cc55892e6410c4894d0d0a808301804c3bfa7af
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-16 16:07:07 +0100

    add more tests

--- units/test_rl_space.cpp
+++ units/test_rl_space.cpp
@@ -1,3 +1,4 @@
+#include <msode/core/math.h>
 #include <msode/rl/space.h>
 
 #include <gtest/gtest.h>
@@ -28,6 +29,51 @@ GTEST_TEST( RL_SPACE, box_samples_are_inside )
             ASSERT_GE(r.x, -L);
             ASSERT_GE(r.y, -L);
             ASSERT_GE(r.z, -L);
+        }
+    }
+}
+
+GTEST_TEST( RL_SPACE, ball_samples_are_inside )
+{
+    std::mt19937 gen(4242);
+    const real R {25.0_r};
+
+    const int nPos = 16;
+    const long nSamples = 1000;
+
+    rl::EnvSpaceBall space(R);
+    
+    for (int i = 0; i < nSamples; ++i)
+    {
+        const auto positions = space.generateNewPositions(gen, nPos);
+
+        for (auto r : positions)
+        {
+            ASSERT_LE(dot(r,r), R*R);
+        }
+    }
+}
+
+GTEST_TEST( RL_SPACE, ball_curriculum_state_samples_are_inside )
+{
+    std::mt19937 gen(4242);
+    const real R {25.0_r};
+    const real targetRadius {2.0_r};
+    const real sigmaRW = targetRadius * 0.1_r;
+
+    const int nPos = 16;
+    const long nSamples = 1000;
+
+    rl::EnvSpaceBallCuriculumStateRW space(R, targetRadius, sigmaRW);
+    
+    for (int i = 0; i < nSamples; ++i)
+    {
+        const auto positions = space.generateNewPositions(gen, nPos);
+
+        for (auto r : positions)
+        {
+            // ASSERT_GE(dot(r,r), targetRadius*targetRadius);
+            ASSERT_LE(dot(r,r), R*R);
         }
     }
 }
