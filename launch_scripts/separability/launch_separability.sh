72f876d0a6722fa0abefe3e6f715ac95a80c71c1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-17 08:44:28 +0100

    adapt script to new path

--- launch_scripts/separability/launch_separability.sh
+++ launch_scripts/separability/launch_separability.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-base="sep"
+apps_bin=../../build/apps/
+result_base_name="results/sep"
 nsamples=1000
 
 ae () { awk 'BEGIN {printf "%g", '"$@"'}'; }
 
 for N in `seq 2 12`; do
-    omegaMax=`ae "$N * 25.0"`
-    echo "starting N = $N with wmax = $omegaMax"
-    ./app_ac_separability $N $omegaMax $nsamples > ${base}_N_${N}.dat
+    omega_max=`ae "$N * 25.0"`
+    echo "starting N = $N with wmax = $omega_max"
+    $apps_bin/app_ac_separability $N $omega_max $nsamples > ${result_base_name}_N_${N}.dat
     echo done
 done
