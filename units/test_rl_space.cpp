dbe53d70f8b20d1f05dd7d2427bb1f66145fda95
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-03-04 10:48:29 +0100

    add test for the new drift curriculum space

--- units/test_rl_space.cpp
+++ units/test_rl_space.cpp
@@ -1,11 +1,14 @@
 #include <msode/core/math.h>
+#include <msode/core/velocity_field/constant.h>
 #include <msode/rl/space/factory.h>
 #include <msode/rl/space/box.h>
 #include <msode/rl/space/ball.h>
 #include <msode/rl/space/ball_curriculum_state.h>
+#include <msode/rl/space/ball_curriculum_state_drift.h>
 
 #include <gtest/gtest.h>
 #include <cmath>
+#include <memory>
 
 using namespace msode;
 
@@ -81,6 +84,44 @@ GTEST_TEST( RL_SPACE, ball_curriculum_state_samples_are_inside )
             ASSERT_GE(dot(r,r), targetRadius*targetRadius);
             ASSERT_LE(dot(r,r), R*R);
         }
+    }
+}
+
+GTEST_TEST( RL_SPACE, ball_curriculum_state_drift_correct_drift )
+{
+    std::mt19937 gen(4242);
+    const real R {25.0_r};
+    const real targetRadius {2.0_r};
+    const real sigmaRW = 0.0_r; // no rw in this case
+
+    const real3 vel {3.0_r, 4.0_r, 5.0_r};
+    const real driftTime {1.2_r};
+
+    // otherwise we might end up in an infinite loop
+    ASSERT_GE(length(vel) * driftTime, 2*targetRadius);
+    ASSERT_LE(length(vel) * driftTime, R - targetRadius);
+
+    const int nPos = 16;
+    const long nSamples = 1000;
+
+    constexpr int maxTries = 1;
+    rl::EnvSpaceBallCurriculumStateDriftRW space(maxTries, R, targetRadius, sigmaRW,
+                                                 std::make_unique<VelocityFieldConstant>(vel),
+                                                 driftTime);
+    
+    const auto initPos = space.generateNewPositions(gen, nPos);
+    const auto nextPos = space.generateNewPositions(gen, nPos);
+
+    for (size_t i = 0; i < initPos.size(); ++i)
+    {
+        constexpr real tol = 1e-6_r;
+        
+        const real3 refDrift = -driftTime * vel;
+        const real3 drift = nextPos[i] - initPos[i];
+
+        ASSERT_NEAR(drift.x, refDrift.x, tol);
+        ASSERT_NEAR(drift.y, refDrift.y, tol);
+        ASSERT_NEAR(drift.z, refDrift.z, tol);
     }
 }
 
