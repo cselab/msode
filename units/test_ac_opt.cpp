1eaf5437609d5d176284a0b197c2ef58b1b62c2f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-03-17 13:27:40 +0100

    compute gradient unit test (wip)

--- units/test_ac_opt.cpp
+++ units/test_ac_opt.cpp
@@ -37,17 +37,78 @@ static std::vector<real3> generateA(int n, std::mt19937& gen)
 
 static inline Quaternion quaternionFromAngles(real theta, real phi, real psi)
 {
-    const real3 normal {std::cos(theta) * std::sin(phi),
-                        std::sin(theta) * std::sin(phi),
-                        std::cos(phi)};
+    const real3 normal {std::cos(phi) * std::sin(psi),
+                        std::sin(phi) * std::sin(psi),
+                        std::cos(psi)};
 
-    const auto q = Quaternion::createFromRotation(psi, normal);
+    const auto q = Quaternion::createFromRotation(theta, normal);
     return q.normalized();
+}
+
+template <typename T>
+int sgn(T val)
+{
+    return (T(0) < val) - (val < T(0));
+}
+
+static real3 computeGradient(const std::vector<real3>& A, real theta, real phi, real psi)
+{
+    const real ct = std::cos(theta);
+    const real st = std::sin(theta);
+
+    const real omct = 1.0_r - ct;
+    
+    const real cph = std::cos(phi);
+    const real sph = std::sin(phi);
+
+    const real cps = std::cos(psi);
+    const real sps = std::sin(psi);
+    
+    const real3 u {cph * sps, sph * sps, cps};
+
+    // The rotation matrix columns
+
+    const real3 R0 {u.x * u.x * omct + ct,         u.y * u.x * omct + u.z * st,   u.z * u.x * omct - u.y * st};
+    const real3 R1 {u.x * u.y * omct - u.z * st,   u.y * u.y * omct + ct,         u.z * u.y * omct + u.x * st};
+    const real3 R2 {u.x * u.z * omct + u.y * st,   u.y * u.z * omct - u.x * st,   u.z * u.z * omct + ct      };
+    
+    // derivatives of the columns of the rotation matrix w.r.t. theta, phi and psi
+    
+    const real3 Rth0 {u.x * u.x * st - st,         u.y * u.x * st + u.z * ct,   u.z * u.x * st - u.y * ct};
+    const real3 Rth1 {u.x * u.y * st - u.z * ct,   u.y * u.y * st - st,         u.z * u.y * st + u.x * ct};
+    const real3 Rth2 {u.x * u.z * st + u.y * ct,   u.y * u.z * st - u.x * ct,   u.z * u.z * st - st      };
+
+    const real3 Rph0 {0.0_r, 0.0_r, 0.0_r};
+    const real3 Rph1 {0.0_r, 0.0_r, 0.0_r};
+    const real3 Rph2 {0.0_r, 0.0_r, 0.0_r};
+
+    const real3 Rps0 {0.0_r, 0.0_r, 0.0_r};
+    const real3 Rps1 {0.0_r, 0.0_r, 0.0_r};
+    const real3 Rps2 {0.0_r, 0.0_r, 0.0_r};
+
+    real3 gradient {0.0_r, 0.0_r, 0.0_r};
+
+    for (auto a : A)
+    {
+        const real aR0 = dot(a, R0);
+        const real aR1 = dot(a, R1);
+        const real aR2 = dot(a, R2);
+
+        // theta
+        gradient.x += sgn(aR0) * dot(a, Rth0) + sgn(aR1) * dot(a, Rth1) + sgn(aR2) * dot(a, Rth2);
+        // phi
+        gradient.y += sgn(aR0) * dot(a, Rph0) + sgn(aR1) * dot(a, Rph1) + sgn(aR2) * dot(a, Rph2);
+        // psi
+        gradient.z += sgn(aR0) * dot(a, Rps0) + sgn(aR1) * dot(a, Rps1) + sgn(aR2) * dot(a, Rps2);
+    }
+
+    return gradient;
 }
 
 GTEST_TEST( AC_OPT, derivative )
 {
     const int n = 4;
+    const int numTries = 5;
     std::mt19937 gen {4217};
     const auto A = generateA(n, gen);
 
@@ -56,18 +117,29 @@ GTEST_TEST( AC_OPT, derivative )
         return analytic_control::computeTime(A, quaternionFromAngles(theta, phi, psi));
     };
 
-    // TODO random tries
-    const real theta {0.34545_r};
-    const real phi {0.14545_r};
-    const real psi {0.64545_r};
-
-    const real h = 1e-3_r;
+    std::uniform_real_distribution<real> dstr(0.0_r, 1.0_r);
     
-    // derivative wrt theta
-    const real dFdtheta_FD = (F(theta + h, phi, psi) - F(theta, phi, psi)) / h;
+    for (int i = 0; i < numTries; ++i)
+    {
+        const real theta = dstr(gen) * 2 * M_PI;
+        const real phi   = dstr(gen) * 2 * M_PI;
+        const real psi   = dstr(gen) * 1 * M_PI;
 
-    // TODO
-    printf("%g\n", dFdtheta_FD);
+        const real h = 1e-4_r;
+        const real tolerance = 1e-3_r;
+
+        const real F0 = F(theta, phi, psi);
+        const real3 gradient_FD = {(F(theta + h, phi, psi) - F(theta - h, phi, psi)) / (2*h),
+                                   (F(theta, phi + h, psi) - F0) / h,
+                                   (F(theta, phi, psi + h) - F0) / h};
+
+        const real3 gradient = computeGradient(A, theta, phi, psi);
+
+        ASSERT_NEAR(gradient_FD.x, gradient.x, tolerance);
+
+        // TODO
+        printf("%g  %g\n", gradient_FD.x, gradient.x);
+    }
 }
 
 int main(int argc, char **argv)
