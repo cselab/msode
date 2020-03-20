ff45f29fe431bd49326a13a9dc23c01b4fe0947d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-03-20 14:19:17 +0100

    add backtrace for debugging

--- src/msode/core/log.cpp
+++ src/msode/core/log.cpp
@@ -1,9 +1,33 @@
 #include "log.h"
 
+#define BACKWARD_HAS_BFD 1
+#include "../../../extern/backward-cpp/backward.hpp"
+
+#include <iostream>
 #include <stdarg.h>
 
 namespace msode
 {
+
+backward::SignalHandling sh;
+
+static void getStacktrace(std::ostream& stream, size_t traceCntMax = 100)
+{
+    using namespace backward;
+
+    StackTrace st;
+    st.load_here(traceCntMax);
+    Printer p;
+    p.object = true;
+    p.color_mode = ColorMode::automatic;
+    p.address = true;
+    p.print(st, stream);
+}
+
+void printStacktrace()
+{
+    getStacktrace(std::cerr);
+}
 
 void die__(const char *file, int line, const char* pattern, ...)
 {
@@ -15,6 +39,9 @@ void die__(const char *file, int line, const char* pattern, ...)
     va_end(args);
 
     fprintf(stderr, "\n\n");
+
+    printStacktrace();
+
     exit(1);
 }
 
--- src/msode/core/log.h
+++ src/msode/core/log.h
@@ -9,12 +9,14 @@ namespace msode
 {
 void die__ [[gnu::format(printf, 3, 4)]] 
 (const char *file, int line, const char* pattern, ...);
+void printStacktrace();
 } // namespace msode
 
 #ifdef MSODE_FAIL_ON_CONTRACT
 #define MSODE_CHECK(val, msg) do {                   \
         if (!(val)) {                           \
             std::cerr << (msg) << std::endl;    \
+            printStacktrace();                  \
             exit(1);                            \
         }                                       \
     } while(0)
