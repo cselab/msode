91b2b909bae63457b1c2c14414699e1503bb42e7
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-03-23 13:56:58 +0100

    wip: adding robustness test

--- units/test_ac_opt.cpp
+++ units/test_ac_opt.cpp
@@ -1,9 +1,9 @@
 #include "helpers.h"
 #include "timer.h"
 
-
 #include <msode/analytic_control/helpers.h>
 #include <msode/analytic_control/optimal_path.h>
+#include <msode/core/factory.h>
 
 #include <cstdio>
 #include <gtest/gtest.h>
@@ -117,6 +117,131 @@ GTEST_TEST( AC_OPT, optimum )
     // check that the approximate optimizer is within 1% bounds
     ASSERT_NEAR(ttCMAES, ttLBFGS, 0.01_r * std::abs(ttCMAES));
 }
+
+
+// case that caused troubles in previous cmaes version
+static std::vector<real3> computeADifficultCase()
+{
+    const Config config = json::parse(R"(
+{
+"bodies" : [
+{
+    "moment": [0.0, 10.0, 0.0],
+    "quaternion": [0.0, 1.0, 0.0, 0.0],
+    "position": [0.0, 0.0, 0.0],
+    "propulsion": {
+        "A": [0.2463907364779919, 0.20047533416665167, 0.19963144341455386],
+        "B": [0.1, 0.0, 0.0],
+        "C": [6.9836362589703, 1.1539878672490023, 1.2181219967949395]
+    }
+},
+{
+    "moment": [0.0, 3.0356727390869245, 0.0],
+    "quaternion": [0.0, 1.0, 0.0, 0.0],
+    "position": [0.0, 0.0, 0.0],
+    "propulsion": {
+        "A": [0.3239313882752511, 0.2413293038414949, 0.2543042668817562],
+        "B": [0.32941627307981225, 0.0, 0.0],
+        "C": [9.43265794343002, 1.681798137390105, 1.7429895150200194]
+    }
+}
+]
+})");
+
+    std::vector<RigidBody> bodies;
+    for (auto c : config.at("bodies"))
+        bodies.push_back(factory::readRigidBodyFromConfig(c));
+
+    const std::vector<real3> positions {real3{-6.82644, 5.90076, 1.85192},
+                                        real3{0.976324, 3.19717, 3.25685}};
+
+    const auto V = analytic_control::createVelocityMatrix(magneticFieldMagnitude, bodies);
+    const auto U = V.inverse();
+    return analytic_control::computeA(U, positions);
+}
+
+// GTEST_TEST( AC_OPT, korali_comparison )
+// {
+//     const auto A = computeADifficultCase();
+
+//     const bool verbose = false;
+    
+//     for (long seed = 4242425; seed < 4242435; ++seed)
+//     {
+//         printf("\nseed %ld:\n", seed);
+//         auto qKorali = analytic_control::findBestPathCMAESKorali(A, seed, verbose);
+//         const real ttKorali = analytic_control::computeTravelTime(A, qKorali);
+//         printf("korali: %g\n", ttKorali);
+           
+//         auto qCMAES = analytic_control::findBestPathCMAES(A, seed, verbose);
+//         const real ttCMAES = analytic_control::computeTravelTime(A, qCMAES);
+//         printf("CMAES:  %g\n", ttCMAES);
+//     }
+// }
+
+
+GTEST_TEST( AC_OPT, robustness )
+{
+    const auto A = computeADifficultCase();
+
+    const bool verbose = false;
+    const int numTries = 1000;
+    std::mt19937 gen {42424242};
+
+    std::vector<real> results;
+    results.reserve(numTries);
+    
+    for (int i = 0; i < numTries; ++i)
+    {
+        const long seed = gen();
+        auto q = analytic_control::findBestPathCMAES(A, seed, verbose);
+        const real tt = analytic_control::computeTravelTime(A, q);
+        results.push_back(tt);
+    }
+
+    const real best = *std::min_element(results.begin(), results.end());
+    const real tol = 1e-3;
+    int numFailed = 0;
+    for (auto v : results)
+    {
+        if (v > best + tol)
+            ++numFailed;
+    }
+
+    printf("%d failed (%g%)\n", numFailed, 100.0 * (double) numFailed / (double) numTries);
+}
+
+// GTEST_TEST( AC_OPT, robustnessKorali )
+// {
+//     const auto A = computeADifficultCase();
+
+//     const bool verbose = false;
+//     const int numTries = 1000;
+//     std::mt19937 gen {42424242};
+
+//     std::vector<real> results;
+//     results.reserve(numTries);
+    
+//     for (int i = 0; i < numTries; ++i)
+//     {
+//         const long seed = gen();
+//         auto q = analytic_control::findBestPathCMAESKorali(A, seed, verbose);
+//         const real tt = analytic_control::computeTravelTime(A, q);
+//         results.push_back(tt);
+//     }
+
+//     const real best = *std::min_element(results.begin(), results.end());
+//     const real tol = 1e-3;
+//     int numFailed = 0;
+//     for (auto v : results)
+//     {
+//         if (v > best + tol)
+//             ++numFailed;
+//     }
+
+//     printf("%d failed (%g%)\n", numFailed, 100.0 * (double) numFailed / (double) numTries);
+// }
+
 
 int main(int argc, char **argv)
 {
