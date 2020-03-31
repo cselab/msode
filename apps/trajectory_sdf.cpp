bde9777639c6c1566acc709b8bca99ffe0cdac49
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-03-31 11:33:40 +0200

    dump field along with sdf; this allow visit to filter velocity and vorticity fields w.r.t. distance

--- apps/trajectory_sdf.cpp
+++ apps/trajectory_sdf.cpp
@@ -1,12 +1,13 @@
 /** trajectory_sdf.cpp
     
-    Compute the distance field of a set of ABF trajectories
+    Compute the distance field of a set of ABF trajectories along with the velocity and vorticity fields
  */
 
 #include "utils.h"
 
 #include <msode/core/factory.h>
 #include <msode/core/simulation.h>
+#include <msode/core/velocity_field/factory.h>
 #include <msode/rl/pos_ic/factory.h>
 
 #include <iostream>
@@ -72,7 +73,7 @@ int main(int argc, char **argv)
 {
     if (argc != 4)
     {
-        fprintf(stderr, "usage : %s <config.json> <trajectory.dat> <sdf.vtk>\n\n", argv[0]);
+        fprintf(stderr, "usage : %s <config.json> <trajectory.dat> <field_and_sdf.vtk>\n\n", argv[0]);
         return 1;
     }
 
@@ -85,13 +86,14 @@ int main(int argc, char **argv)
 
     const real magneticFieldMagnitude = config.at("fieldMagnitude").get<real>();
     const auto bodies = app_utils::readBodies(config.at("bodies"));
+    auto field = factory::createVelocityField(config.at("velocityField"));
     auto posIc = rl::factory::createEnvPosIC(config.at("posIc"));
 
     const auto trajectory = app_utils::readTrajectory(bodies, argv[2]);
     const auto allSegments = extractSegments(trajectory);
     
     // values on the grid
-    
+
     const int n = 64;
     const int3 res {n, n, n};
     const real3 start = posIc->getLowestPosition();
@@ -117,7 +119,7 @@ int main(int argc, char **argv)
             }
         }
     }
-
+    
     // dump to vtk
 
     const char *outFileName = argv[3];
@@ -125,17 +127,10 @@ int main(int argc, char **argv)
 
     MSODE_Ensure(f.is_open(), "Error opening file '%s'", outFileName);
 
-    f << "# vtk DataFile Version 2.0\n"
-      << "Distance to trajectories dumped by msode\n"
-      << "ASCII\n";
-    
-    f << "DATASET STRUCTURED_POINTS\n"
-      << "DIMENSIONS " << res.x << ' ' << res.y << ' ' << res.z << '\n'
-      << "ORIGIN " << start.x << ' ' << start.y << ' ' << start.z << '\n'
-      << "SPACING " << h.x << ' ' << h.y << ' ' << h.z << '\n';
+    const real time {0.0_r};
+    field->dumpToVtkUniformGrid(f, res, start, size, time);
 
-    f << "POINT_DATA " << vals.size() << "\n"
-      << "SCALARS sdf float 1\n"
+    f << "SCALARS sdf float 1\n"
       << "LOOKUP_TABLE default\n";
 
     for (auto v : vals)
