4cdf613b3684986e0f222d9a72ce48c16fffd0cb
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-09-21 16:09:39 +0200

    test for json pointer

--- units/test_config.cpp
+++ units/test_config.cpp
@@ -13,11 +13,11 @@ GTEST_TEST( CONFIG, propulsion_matrix )
     p1.A[0] = 1.0_r;
     p1.A[1] = 2.0_r;
     p1.A[2] = 3.0_r;
-    
+
     p1.B[0] = 4.0_r;
     p1.B[1] = 5.0_r;
     p1.B[2] = 6.0_r;
-    
+
     p1.C[0] = 7.0_r;
     p1.C[1] = 8.0_r;
     p1.C[2] = 9.0_r;
@@ -34,6 +34,21 @@ GTEST_TEST( CONFIG, propulsion_matrix )
         ASSERT_EQ(p1.B[i], p2.B[i]);
         ASSERT_EQ(p1.C[i], p2.C[i]);
     }
+}
+
+GTEST_TEST( CONFIG, str2quaternion )
+{
+    Config config = json::parse(R"(
+    {
+        "q" : [1, 2, 3, 4]
+    })");
+
+    const auto q = config.at("q").get<Quaternion>();
+
+    ASSERT_EQ(q.w, 1.0_r);
+    ASSERT_EQ(q.x, 2.0_r);
+    ASSERT_EQ(q.y, 3.0_r);
+    ASSERT_EQ(q.z, 4.0_r);
 }
 
 GTEST_TEST( CONFIG, quaternion )
@@ -62,6 +77,25 @@ GTEST_TEST( CONFIG, real3 )
     ASSERT_EQ(r1.y, r2.y);
     ASSERT_EQ(r1.z, r2.z);
 }
+
+GTEST_TEST( CONFIG, pointer )
+{
+    Config config = json::parse(R"(
+    {
+        "main" : {
+            "a" : 1,
+            "b" : 2,
+            "ptrb" : "/main/b"
+        }
+    })");
+
+    json::json_pointer ptra("/main/a");
+    json::json_pointer ptrb(config.at("main").at("ptrb"));
+
+    ASSERT_EQ(config.at(ptra).get<int>(), 1);
+    ASSERT_EQ(config.at(ptrb).get<int>(), 2);
+}
+
 
 int main(int argc, char **argv)
 {
