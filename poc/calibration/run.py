513b97daaf914db2cec4c24dca87fa480a87912f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-09-21 11:19:26 +0200

    use cheaper model

--- poc/calibration/run.py
+++ poc/calibration/run.py
@@ -5,38 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-import sys
 
-sys.path.insert(0, os.path.join('..', '1D'))
-from model import ABFs
-
-def compute_V(bmb: float,
-              cmb: float,
-              omega: float,
-              tend: float=50):
-
-    x0 = [0]
-    abf = ABFs(x0, [bmb], [cmb])
-    abf.advance(omega, tend)
-    V = abf.x[0] / tend
-    return V
-
-def plot_comparison(fname: str,
-                    bmb: float,
-                    cmb: float):
-    df = pd.read_csv(fname)
-    omegas = df['omega'].to_numpy()
-    Vexp = df['V'].to_numpy()
-
-    V = [compute_V(bmb, cmb, w) for w in omegas]
-
-    fig, ax = plt.subplots()
-
-    ax.plot(omegas, Vexp, '+')
-    ax.plot(omegas, V, '-')
-
-    plt.show()
-
+from model import compute_V
 
 def infer_TMCMC(fname: str,
                 use_MPI: bool=False):
@@ -64,7 +34,7 @@ def infer_TMCMC(fname: str,
 
     # Configuring TMCMC parameters
     e["Solver"]["Type"] = "Sampler/TMCMC"
-    e["Solver"]["Population Size"] = 5000
+    e["Solver"]["Population Size"] = 1000
 
     # Configuring the problem's random distributions
     e["Distributions"][0]["Name"] = "Uniform 0"
@@ -110,4 +80,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     infer_TMCMC(args.data, args.mpi)
-    #plot_comparison(args.data, 10, 10)
