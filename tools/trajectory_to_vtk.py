407401f1424d1f8e3cb12668b16a00b458c7e3ab
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-09-29 15:28:09 +0200

    minor: more pythonesk syntax

--- tools/trajectory_to_vtk.py
+++ tools/trajectory_to_vtk.py
@@ -21,64 +21,65 @@ def write_point_data_vector(f, point_data, name):
     for val in point_data:
         f.write("{} {} {}\n".format(val[0], val[1], val[2]))
 
+def main(argv):
+    parser = argparse.ArgumentParser(description="Create a vtk file from the trajectory files output by an msode simulation")
+    parser.add_argument('file',         type=str, help='output of ODE simulation')
+    parser.add_argument('out_basename', type=str, help='vtk file output base name, will be basename.<swimmer id>.vtk')
+    parser.add_argument('--with_field_from', type=str, default=None, metavar='config.json',
+                        help='if set, the background velocity field will be evaluated at every point of the trajectory. \
+                        The velocity field is described in the json file passed in this argument.')
+    args = parser.parse_args(argv)
 
-parser = argparse.ArgumentParser(description="Create a vtk file from the trajectory files output by an msode simulation")
-parser.add_argument('file',         type=str, help='output of ODE simulation')
-parser.add_argument('out_basename', type=str, help='vtk file output base name, will be basename.<swimmer id>.vtk')
-parser.add_argument('--with_field_from', type=str, default=None, metavar='config.json',
-                    help='if set, the background velocity field will be evaluated at every point of the trajectory. \
-                    The velocity field is described in the json file passed in this argument.')
-args = parser.parse_args()
+    data = np.loadtxt(args.file)
 
-data = np.loadtxt(args.file)
+    ncolumnds_per_rigid = 4 + 3 + 3 # q, r, w
+    ncolumns = len(data[0,:])
+    nrigids = (ncolumns-1) // ncolumnds_per_rigid
 
-ncolumnds_per_rigid = 4 + 3 + 3 # q, r, w
-ncolumns = len(data[0,:])
-nrigids = (ncolumns-1) // ncolumnds_per_rigid
+    time = data[:,0]
+    omega_field = data[:,1]
 
-time = data[:,0]
-omega_field = data[:,1]
+    start_base = 5 # time, omega direction, omega magnitude
 
-start_base = 5 # time, omega direction, omega magnitude
+    for i in range(nrigids):
+        start = start_base + i * ncolumnds_per_rigid
+        end   = start + ncolumnds_per_rigid
+        q, pos, omega = read_rigid_data(data[:, start:end])
 
-for i in range(nrigids):
+        x = pos[:,0]
+        y = pos[:,1]
+        z = pos[:,2]
+        n = len(x)
 
-    start = start_base + i * ncolumnds_per_rigid
-    end   = start + ncolumnds_per_rigid
-    q, pos, omega = read_rigid_data(data[:, start:end])
+        filename = args.out_basename + ".{:04d}.vtk".format(i)
 
-    x = pos[:,0]
-    y = pos[:,1]
-    z = pos[:,2]
-    n = len(x)
+        with open(filename, 'w') as f:
+            f.write('# vtk DataFile Version 2.0\n')
+            f.write('generated from msode trajectories\n')
+            f.write('ASCII\n')
+            f.write('DATASET POLYDATA\n')
+            f.write('POINTS {} FLOAT\n'.format(n))
 
-    filename = args.out_basename + ".{:04d}.vtk".format(i)
+            for j in range(n):
+                f.write("{} {} {}\n".format(x[j], y[j], z[j]))
 
-    f = open(filename, 'w')
-    f.write('# vtk DataFile Version 2.0\n')
-    f.write('generated from msode trajectories\n')
-    f.write('ASCII\n')
-    f.write('DATASET POLYDATA\n')
-    f.write('POINTS {} FLOAT\n'.format(n))
+            nl = n-1
+            f.write('LINES {} {}\n'.format(nl, 3 * nl))
 
-    for j in range(n):
-        f.write("{} {} {}\n".format(x[j], y[j], z[j]))
+            for j in range(n-1):
+                f.write("2 {} {}\n".format(j, j+1))
 
-    nl = n-1
-    f.write('LINES {} {}\n'.format(nl, 3 * nl))
+            f.write('\n')
+            f.write('POINT_DATA {}\n'.format(n))
 
-    for j in range(n-1):
-        f.write("2 {} {}\n".format(j, j+1))
+            write_point_data_scalar(f, time, "time")
+            write_point_data_scalar(f, np.abs(omega_field), "omega")
 
-    f.write('\n')
-    f.write('POINT_DATA {}\n'.format(n))
+            if args.with_field_from is not None:
+                import velocity_field as vf
+                field_vel = vf.evaluate_field_from_config_filename(x, y, z, args.with_field_from)
+                write_point_data_vector(f, field_vel, "velocity_field")
 
-    write_point_data_scalar(f, time, "time")
-    write_point_data_scalar(f, np.abs(omega_field), "omega")
-
-    if args.with_field_from is not None:
-        import velocity_field as vf
-        field_vel = vf.evaluate_field_from_config_filename(x, y, z, args.with_field_from)
-        write_point_data_vector(f, field_vel, "velocity_field")
-
-    f.close()
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
