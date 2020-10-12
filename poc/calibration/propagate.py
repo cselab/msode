8bca1ed4df9b9c2f972c75995eb98aa29bd322a4
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-10-12 16:48:04 +0200

    compute the confidence intervals from cdf

--- poc/calibration/propagate.py
+++ poc/calibration/propagate.py
@@ -6,15 +6,14 @@ import numpy as np
 import os
 import pandas as pd
 from model import compute_V
+from scipy.stats import norm
 
 def get_samples(fname: str):
-
     with open(fname) as json_file:
         e = json.load(json_file)
         samples = np.array(e["Results"]["Sample Database"])
 
     return samples
-
 
 def propagate(fname: str,
               samples: np.ndarray):
@@ -27,19 +26,26 @@ def propagate(fname: str,
     ws = np.linspace(0, 1.2*np.max(wexp), nws)
 
     all_Vs = np.zeros((nsamples, nws))
+    all_sigmas = np.zeros(nsamples)
 
     for i, sample in enumerate(samples):
         bmb, cmb, sigma = sample
         all_Vs[i,:] = [compute_V(bmb, cmb, w) for w in ws]
-        all_Vs[i,:] += np.random.normal(loc=0, scale=sigma, size=nws)
+        all_sigmas = sigma
 
+
+    # compute confidence intervals:
+    # discretize the axis, compute CDF and invert at percentiles
+    y = np.linspace(0, 1.5 * np.max(Vexp), 5000)
+
+    cdf = np.sum( [norm.cdf(y_, loc=all_Vs, scale=all_sigmas) for y_ in y], axis=1 ) / nsamples
+
+    Vlo = y[np.argmin(np.abs(cdf - 0.05), axis=0)]
+    Vhi = y[np.argmin(np.abs(cdf - 0.95), axis=0)]
 
     Vmean = np.mean(all_Vs, axis=0)
-    Vhi = np.percentile(all_Vs, q=95, axis=0)
-    Vlo = np.percentile(all_Vs, q=5, axis=0)
 
     fig, ax = plt.subplots()
-
 
     ax.plot(ws, Vmean, '-')
     ax.fill_between(ws, Vlo, Vhi, alpha=0.25)
