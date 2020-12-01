0223482dad80d34e0da4e70ea7839373bfeea19b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-12-01 09:23:17 +0100

    wip: add forward script launcher

---
+++ launch_scripts/forward/x_vs_t.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+apps=../../build/apps
+config=../../data/helix/config/swimmer_00.json
+fps=120
+
+res=results
+
+mkdir -p $res
+
+for w in 40 60 80 100 120; do
+    $apps/forward $config $fps $w $res/xt_w_$w.dat
+done
