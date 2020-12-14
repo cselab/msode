1912da7ab3109c1a4f71a220267b1891adcbdd03
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-12-14 10:45:40 +0100

    use seq

--- launch_scripts/rl/test_robustness.sh
+++ launch_scripts/rl/test_robustness.sh
@@ -7,7 +7,7 @@ res_dir="res_robustness"
 
 mkdir -p $res_dir
 
-for s in 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6; do
+for s in `seq 0.25 0.5 1.75`; do
     conf=config_s_$s.json
     simdir=results/eval_flow_$s
     echo $conf
